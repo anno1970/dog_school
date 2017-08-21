ffcc8af671511a218a93c3262f2aad90bc10efc0
土井惠 <doimegumi@doimeguminoMacBook.local>
2017-08-21 22:00:02 +0900

    Tweetテーブルに４カラム追加・imageカラム削除、エラーの修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    Tweet.create(text: tweet_params[:text], user_id: current_user.id)
   end
 
   def destroy

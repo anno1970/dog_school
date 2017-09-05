6417ac6cfb21b6c30e62f63bd2f0ddb37038708d
土井惠 <doimegumi@doimeguminoMacBook.local>
2017-09-05 19:53:53 +0900

    usersテーブルのnicknameのnull => falseとunique => trueの設定

--- db/migrate/20150506061323_add_nickname_to_users.rb
+++ db/migrate/20150506061323_add_nickname_to_users.rb
@@ -1,5 +1,5 @@
 class AddNicknameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :nickname, :string, :null => false, :unique => true
   end
 end

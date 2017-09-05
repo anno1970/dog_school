cbd76cdc130e650fbb68b35d24875f899f7aa27e
土井惠 <doimegumi@doimeguminoMacBook.local>
2017-09-05 19:33:58 +0900

    null => falseの設定

--- db/migrate/20170905092532_add_name_to_users.rb
+++ db/migrate/20170905092532_add_name_to_users.rb
@@ -1,15 +1,15 @@
 class AddNameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :first_name_kanji, :string
-    add_column :users, :last_name_kanji, :string
-    add_column :users, :first_name_kana, :string
-    add_column :users, :last_name_kana, :string
+    add_column :users, :first_name_kanji, :string, :null => false
+    add_column :users, :last_name_kanji, :string, :null => false
+    add_column :users, :first_name_kana, :string, :null => false
+    add_column :users, :last_name_kana, :string, :null => false
     add_column :users, :sex, :boolean
     add_column :users, :image, :text
     add_column :users, :tel, :string
-    add_column :users, :birthday, :date
+    add_column :users, :birthday, :date, :null => false
     add_column :users, :postal_code, :string
-    add_column :users, :address, :string
+    add_column :users, :address, :string, :null => false
     add_column :users, :facebook_id, :string
     add_column :users, :skype_id, :string
     add_column :users, :subscibe, :integer
